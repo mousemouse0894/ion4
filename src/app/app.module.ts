9f7bccddbc1c6a7ed3b9e9ac4bd186e7e1e7b4b3
mousemouse0894 <biohazardking2012@gmail.com>
2019-04-05 14:54:23 +0700

    fa1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,33 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCEwZG_PgCAhXNdbZb6ODilEKf8j1XhyHo",
+  authDomain: "lon4-b3f81.firebaseapp.com",
+  databaseURL: "https://lon4-b3f81.firebaseio.com",
+  projectId: "lon4-b3f81",
+  storageBucket: "lon4-b3f81.appspot.com",
+  messagingSenderId: "317940159288"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    AngularFireDatabase
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+ 
+ constructor(public fb:AngularFireDatabase){
+ }
 
+  ngOnInit(){
+    this.fb.object("/DHT").valueChanges().subscribe((value:any)=>{
+     console.log(value);
+    
+    });1
+  }
 }
+
+
+
+/*
+ this.fb.object("/DHT").valueChanges().toPromise().then((value:any)=>{
+    console.log(value);
+ 
+    });
+*/
