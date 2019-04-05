92ae83b3570a027a87dfb41972586cb03e651912
mousemouse0894 <biohazardking2012@gmail.com>
2019-04-05 18:12:36 +0700

    fa2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,29 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/database';
+import { Component, OnInit } from "@angular/core";
+import { AngularFireDatabase } from "@angular/fire/database";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage implements OnInit {
- 
- constructor(public fb:AngularFireDatabase){
- }
+  constructor(public fb: AngularFireDatabase) {
+  
+  }
 
-  ngOnInit(){
-    this.fb.object("/DHT").valueChanges().subscribe((value:any)=>{
-     console.log(value);
+  ngOnInit() {
+    this.fb
+      .object("/DHT")
+      .valueChanges()
+      .subscribe((value: any) => {
+        console.log(value);
+      });
     
-    });1
   }
 }
-
-
 
 /*
  this.fb.object("/DHT").valueChanges().toPromise().then((value:any)=>{
     console.log(value);
  
+    });
+    -----------------------------------------------------------------------------
+      this.fb.list("/logs").push({
+      Temperature: Math.random(),
+      Humididy: Math.random(),
+      time:`${new Date().getDay()}/${new Date().getMonth()+1}/${new Date().getFullYear()}`
     });
 */
